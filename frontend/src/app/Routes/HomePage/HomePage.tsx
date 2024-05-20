0996cb117988c6e5673487036ab2be5c4ec9c93b
Msuf123 <akshatmalik18t@gmail.com>
2024-05-20 10:02:44 +0530

    Reading images

--- frontend/src/app/Routes/HomePage/HomePage.tsx
+++ frontend/src/app/Routes/HomePage/HomePage.tsx
@@ -1,7 +1,28 @@
+import { useState } from "react"
+
 export default function HomePage(){
+    const [src,setSrc]=useState('')
 return(
     <div>
         <h1>This is a project which discuss about diffrent MiMe types</h1>
+        <img src={src}></img>
+        
+        <form  onSubmit={(e)=>{
+            e.preventDefault()
+            
+            }}>
+        <input type="file" onChange={(e)=>{
+            const file=new FileReader()
+            let finleInfo=e.target.files![0]
+            console.log(finleInfo)
+            file.readAsDataURL(finleInfo)
+            file.onload = function() {
+                let res:string=file.result!.toString();
+                setSrc(()=>res)
+              };
+        }}></input>
+        <button>Upload</button>
+        </form>
     </div>
 )
 }
--- frontend/src/app/Routes/Root/Sub-Components/TopBar/Topbar.tsx
+++ frontend/src/app/Routes/Root/Sub-Components/TopBar/Topbar.tsx
@@ -1,7 +1,7 @@
 export default function TopBar(){
     return(
         <div>
-            <img src="https://avatars.githubusercontent.com/u/88485149?s=400&u=9104c9af37518948f752a7b567bf191ee7c86608&v=4"></img>
+            <img style={{width:'20px'}} src="https://avatars.githubusercontent.com/u/88485149?s=400&u=9104c9af37518948f752a7b567bf191ee7c86608&v=4"></img>
             
         </div>
     )
