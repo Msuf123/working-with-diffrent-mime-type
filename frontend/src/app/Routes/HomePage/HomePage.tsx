79d61df1be88e530f376e9e543dbbca41310f9ed
Msuf123 <akshatmalik18t@gmail.com>
2024-05-20 18:27:54 +0530

    Learnt how to select the files

--- frontend/src/app/Routes/HomePage/HomePage.tsx
+++ frontend/src/app/Routes/HomePage/HomePage.tsx
@@ -1,28 +1,32 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import { blob } from "stream/consumers"
 
 export default function HomePage(){
     const [src,setSrc]=useState('')
+    const [files,setFiles]=useState([''])
+   
 return(
     <div>
         <h1>This is a project which discuss about diffrent MiMe types</h1>
-        <img src={src}></img>
+        {/* <img src={src}></img> */}
         
         <form  onSubmit={(e)=>{
             e.preventDefault()
             
             }}>
-        <input type="file" onChange={(e)=>{
+        <input type="file" multiple={true} onChange={(e)=>{
             const file=new FileReader()
-            let finleInfo=e.target.files![0]
-            console.log(finleInfo)
-            file.readAsDataURL(finleInfo)
-            file.onload = function() {
-                let res:string=file.result!.toString();
-                setSrc(()=>res)
-              };
+            const files=e.target.files
+            let array:string[]=[]
+            for(let i=0; i<files!.length;i++){
+                array.push(files![i].name)
+            }   
+            setFiles(array)    
         }}></input>
         <button>Upload</button>
         </form>
+        {files.map((a,k)=><span style={{display:'block'}} key={k}>Selected files are {a}</span>)}
+        
     </div>
 )
 }
